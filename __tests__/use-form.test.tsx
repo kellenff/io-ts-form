36203d7816889a136147f06596701add18ca0086
Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>
2022-01-15 16:12:07 -0800

    add initial validation via the type option

--- __tests__/use-form.test.tsx
+++ __tests__/use-form.test.tsx
@@ -1,6 +1,7 @@
 import {VFC} from 'react';
 import {useForm} from '../src';
 import {fireEvent, render} from '@testing-library/react';
+import * as t from 'io-ts';
 
 const Update: VFC = () => {
   const {form, update} = useForm({test: ''});
@@ -18,20 +19,71 @@ const Field: VFC = () => {
   return <input data-testid="input" type="text" {...field('test')} />;
 };
 
+const Validation: VFC<{type?: t.Type<any>}> = ({type}) => {
+  const {field, isValid} = useForm({test: ''}, {type});
+
+  return (
+    <input
+      data-testid="input"
+      type="text"
+      onChange={field('test').onChange}
+      value={String(isValid)}
+    />
+  );
+};
+
 describe('useForm', () => {
-  it('should update the form value when update is called', () => {
-    const {getByTestId} = render(<Update />);
+  describe('Values', () => {
+    it('should update the form value when update is called', () => {
+      const {getByTestId} = render(<Update />);
 
-    fireEvent.change(getByTestId('input'), {target: {value: 'test'}});
+      fireEvent.change(getByTestId('input'), {target: {value: 'test'}});
 
-    expect(getByTestId('input').getAttribute('value')).toEqual('test');
+      expect(getByTestId('input').getAttribute('value')).toEqual('test');
+    });
+
+    it('should update inputs created with field', () => {
+      const {getByTestId} = render(<Field />);
+
+      fireEvent.change(getByTestId('input'), {target: {value: 'test'}});
+
+      expect(getByTestId('input').getAttribute('value')).toEqual('test');
+    });
   });
 
-  it('should update inputs created with field', () => {
-    const {getByTestId} = render(<Field />);
+  describe('Validation', () => {
+    it('should always be valid if no type is passed', () => {
+      const {getByTestId} = render(<Validation />);
 
-    fireEvent.change(getByTestId('input'), {target: {value: 'test'}});
+      expect(getByTestId('input').getAttribute('value')).toEqual('true');
 
-    expect(getByTestId('input').getAttribute('value')).toEqual('test');
+      fireEvent.change(getByTestId('input'), {target: {value: 'test'}});
+
+      expect(getByTestId('input').getAttribute('value')).toEqual('true');
+    });
+
+    it('should validate on valid input', () => {
+      const testType = t.type({
+        test: t.literal('test'),
+      });
+
+      const {getByTestId} = render(<Validation type={testType} />);
+
+      fireEvent.change(getByTestId('input'), {target: {value: 'test'}});
+
+      expect(getByTestId('input').getAttribute('value')).toEqual('true');
+    });
+
+    it('should set isValid to false on invalid input', () => {
+      const testType = t.type({
+        test: t.literal('test'),
+      });
+
+      const {getByTestId} = render(<Validation type={testType} />);
+
+      fireEvent.change(getByTestId('input'), {target: {value: 'tent'}});
+
+      expect(getByTestId('input').getAttribute('value')).toEqual('false');
+    });
   });
 });
