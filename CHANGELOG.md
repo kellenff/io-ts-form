2437e10e3894202edc42b1baa1a8cfb0988ccb81
Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>
2022-01-16 15:18:32 -0800

    Add codec support for form updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,18 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Support for `io-ts/Codec/Codec` to validate and conform the form's values
+
+### Changed
+
+- Replace initial form values with form codec
+
+### Removed
+
+- `UseFormValue.setIsValid` as imperative management of validity state is removed
+
 ## [0.1.0] - 2022-01-15
 
 ### Added
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,62 @@
-import {FormEvent, useEffect, useState} from 'react';
+import {FormEvent, HTMLProps, useState} from 'react';
 import * as t from 'io-ts';
+import * as C from 'io-ts/Codec';
+import * as D from 'io-ts/Decoder';
+import * as E from 'fp-ts/Either';
 
-const htmlInputExtractor = (e: FormEvent<HTMLInputElement>) => e.currentTarget.value;
+const htmlInputExtractor = (e: FormEvent<HTMLInputElement>): HTMLInputElement['value'] =>
+  e.currentTarget.value;
 
-export type UseFormOptions<V> = {
-  type?: t.Type<V>;
+type UseFormValue<V extends object, K extends keyof V> = {
+  form: Partial<FormData<K>>;
+  data: V | null;
+  setForm: (data: FormData<K>) => void;
+  field: (name: K) => Partial<HTMLProps<HTMLInputElement>>;
+  update: (name: K) => (event: FormEvent<HTMLInputElement>) => void;
+  isValid: boolean;
 };
 
-export const useForm = <V extends object>(init: V, options?: UseFormOptions<V>) => {
-  const [form, setForm] = useState<V>(init);
-  const [isValid, setIsValid] = useState(options?.type?.is(init) ?? true);
+export type FormData<K extends string | number | symbol> = Record<K, HTMLInputElement['value']>;
 
-  useEffect(() => {
-    if (options?.type) {
-      setIsValid(options.type.is(form));
+export function useForm<V extends object, T extends t.OutputOf<t.Type<V>>, K extends keyof V>(
+  codec: C.Codec<unknown, FormData<K>, T>,
+): UseFormValue<V, K> {
+  const [form, setForm] = useState<Partial<FormData<K>>>({});
+  const [error, setError] = useState<D.DecodeError | null>(null);
+  const [data, setData] = useState<T | null>(null);
+
+  const isValid = error === null;
+
+  const handleSetForm = (form: FormData<K>) => {
+    const decodeResult = codec.decode(form);
+    const newForm = E.fold(
+      () => form,
+      (d) => codec.encode(d as T),
+    )(decodeResult);
+    setForm(newForm);
+
+    if (E.isLeft(decodeResult)) {
+      setData(null);
+      setError(decodeResult.left);
+    } else {
+      setData(decodeResult.right);
+      setError(null);
     }
-  }, [options?.type, setIsValid, form]);
+  };
 
   const update =
     <K extends keyof V>(key: K) =>
     (event: FormEvent<HTMLInputElement>) => {
-      setForm({
+      handleSetForm({
         ...form,
         [key]: htmlInputExtractor(event),
       });
     };
 
-  const field = (key: keyof V) => ({
+  const field = (key: K) => ({
     onChange: update(key),
     value: form[key],
   });
 
-  return {form, setForm, field, update, isValid, setIsValid};
-};
+  return {form, setForm: handleSetForm, data, field, update, isValid};
+}
